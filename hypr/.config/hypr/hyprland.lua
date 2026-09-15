ffe743ad2bf482f0aebdd9da11224679d7b4c436
chetran <chelongtran@gmail.com>
2026-09-15 14:08:18 +0200

    hypr: use thunar and add bind

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -42,7 +42,7 @@ hl.monitor({
 
 -- Set programs that you use
 local terminal    = "kitty"
-local fileManager = "dolphin"
+local fileManager = "thunar"
 local menu        = "fuzzel"
 local browser     = "firefox"
 
